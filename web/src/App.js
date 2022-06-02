516f323535df7412771a2db6ca86a47aed690e3a
Rizdent <kulakovsaleksejs1@gmail.com>
2022-06-02 20:20:27 +0300

    Update App.js

--- web/src/App.js
+++ web/src/App.js
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import React from "react";
 import logo from './RizidentMeme.png';
 import './App.css';
@@ -94,5 +95,7 @@ function App() {
       </header>
     </div >
   );
+
 }
-}
+export default App 
+
